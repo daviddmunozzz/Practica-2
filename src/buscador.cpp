6068b23081a16ceec99ef56716cff7df970d18f2
daviddmunozzz <davidescribano2002@gmail.com>
2024-03-15 16:42:41 +0100

    inicializacion buscador

---
+++ src/buscador.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <thread>
+
+
+int main(int argc, char *argv[])
+{
+    if(argc < 4)
+    {
+        std::cout << "Error: Faltan argumentos" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    
+}
+
